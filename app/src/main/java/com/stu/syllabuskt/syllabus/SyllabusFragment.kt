6d9ec3ea2ceafc5ada71ae0232a11dfdad942bf6
yuan <yuansssf@gmail.com>
2020-12-15 11:18:43 +0800

    opt draw grid code

--- app/src/main/java/com/stu/syllabuskt/syllabus/SyllabusFragment.kt
+++ app/src/main/java/com/stu/syllabuskt/syllabus/SyllabusFragment.kt
@@ -151,202 +151,52 @@ class SyllabusFragment : BaseFragment(), ISyllabusContract.IView {
             val endWeek = week[1].toInt()
             if (startWeek > weekIndex || endWeek < weekIndex) continue
 
-            // TODO: 2019/12/5 写了大量重复的代码
-            // TODO: 2020/12/12 还没空优化，再见 
             if (showLessonBean.getDays().getW0() != null && showLessonBean.getDays().getW0() != "-") {
                 Log.d(TAG, "showSyllabus: 周日" + showLessonBean.getDays().getW0())
+                drawGrid(showLessonBean, showLessonBean.getDays().getW0(), 0)
             }
             if (showLessonBean.getDays().getW1() != null && showLessonBean.getDays().getW1() != "-") {
                 Log.d(TAG, "showSyllabus: 周一" + showLessonBean.getDays().getW1())
-                val time: List<String> = showLessonBean.getDays().getW1().split("-")
-                val lessonLinearLayout = LayoutInflater.from(activity)
-                    .inflate(R.layout.lesson_grid, null, false) as MaterialRippleLayout
-                val lessonRippleLayout: MaterialRippleLayout =
-                    lessonLinearLayout.findViewById<View>(R.id.lessonInfoRipple) as MaterialRippleLayout
-                lessonRippleLayout.setOnClickListener(View.OnClickListener {
-                    val days: String =
-                        showLessonBean.getDuration().toString() + "周\n" + showLessonBean.getDays()
-                            .toString()
-//                    mActivity.startActivity(
-//                        LessonInfoActivity.getIntent(
-//                            mActivity,
-//                            showLessonBean.getBgColor(),
-//                            showLessonBean.getName(),
-//                            showLessonBean.getId(),
-//                            showLessonBean.getCredit(),
-//                            showLessonBean.getRoom(),
-//                            showLessonBean.getTeacher(),
-//                            days
-//                        )
-//                    )
-                })
-                val lessonTextView =
-                    lessonLinearLayout.findViewById<View>(R.id.lessonTextView) as TextView
-                lessonTextView.text = showLessonBean.getName().toString() + "\n@" + showLessonBean.getRoom()
-                lessonTextView.width = gridWidth
-                lessonTextView.height = gridHeight * (time[1].toInt() - time[0].toInt() + 1)
-                val shape = resources.getDrawable(R.drawable.grid_background) as GradientDrawable
-                shape.setColor(
-                    ColorUtils.setAlphaComponent(
-                        resources.getColor(
-                            showLessonBean.getBgColor()
-                        ), 192
-                    )
-                )
-                lessonTextView.setBackgroundDrawable(shape)
-                val rowSpec =
-                    GridLayout.spec(time[0].toInt() - 1, time[1].toInt() - time[0].toInt() + 1)
-                val columnSpec = GridLayout.spec(1)
-                gridLayout.addView(lessonLinearLayout, GridLayout.LayoutParams(rowSpec, columnSpec))
+                drawGrid(showLessonBean, showLessonBean.getDays().getW1(), 1)
             }
             if (showLessonBean.getDays().getW2() != null && showLessonBean.getDays().getW2() != "-") {
                 Log.d(TAG, "showSyllabus: 周二" + showLessonBean.getDays().getW2())
-                val time: List<String> = showLessonBean.getDays().getW2().split("-")
-                val lessonLinearLayout = LayoutInflater.from(activity)
-                    .inflate(R.layout.lesson_grid, null, false) as MaterialRippleLayout
-                val lessonRippleLayout: MaterialRippleLayout = lessonLinearLayout.findViewById<View>(R.id.lessonInfoRipple) as MaterialRippleLayout
-                lessonRippleLayout.setOnClickListener(View.OnClickListener {
-                    val days: String =
-                        showLessonBean.getDuration().toString() + "周\n" + showLessonBean.getDays()
-                            .toString()
-//                    mActivity.startActivity(
-//                        LessonInfoActivity.getIntent(
-//                            mActivity,
-//                            showLessonBean.getBgColor(),
-//                            showLessonBean.getName(),
-//                            showLessonBean.getId(),
-//                            showLessonBean.getCredit(),
-//                            showLessonBean.getRoom(),
-//                            showLessonBean.getTeacher(),
-//                            days
-//                        )
-//                    )
-                })
-                val lessonTextView = lessonLinearLayout.findViewById<View>(R.id.lessonTextView) as TextView
-                lessonTextView.text = showLessonBean.getName().toString() + "\n@" + showLessonBean.getRoom()
-                Log.d(TAG, "showSyllabus: " + showLessonBean.getRoom())
-                lessonTextView.width = gridWidth
-                lessonTextView.height = gridHeight * (time[1].toInt() - time[0].toInt() + 1)
-                val shape = resources.getDrawable(R.drawable.grid_background) as GradientDrawable
-                shape.setColor(ColorUtils.setAlphaComponent(resources.getColor(showLessonBean.getBgColor()), 192))
-                lessonTextView.setBackgroundDrawable(shape)
-                val rowSpec = GridLayout.spec(time[0].toInt() - 1, time[1].toInt() - time[0].toInt() + 1)
-                val columnSpec = GridLayout.spec(2)
-                gridLayout.addView(lessonLinearLayout, GridLayout.LayoutParams(rowSpec, columnSpec))
+                drawGrid(showLessonBean, showLessonBean.getDays().getW2(), 2)
             }
             if (showLessonBean.getDays().getW3() != null && showLessonBean.getDays().getW3() != "-") {
                 Log.d(TAG, "showSyllabus: 周三" + showLessonBean.getDays().getW3())
-                val time: List<String> = showLessonBean.getDays().getW3().split("-")
-                val lessonLinearLayout = LayoutInflater.from(activity)
-                    .inflate(R.layout.lesson_grid, null, false) as MaterialRippleLayout
-                val lessonRippleLayout: MaterialRippleLayout = lessonLinearLayout.findViewById<View>(R.id.lessonInfoRipple) as MaterialRippleLayout
-                lessonRippleLayout.setOnClickListener(View.OnClickListener {
-                    val days: String =
-                        showLessonBean.getDuration().toString() + "周\n" + showLessonBean.getDays()
-                            .toString()
-//                    mActivity.startActivity(
-//                        LessonInfoActivity.getIntent(
-//                            mActivity,
-//                            showLessonBean.getBgColor(),
-//                            showLessonBean.getName(),
-//                            showLessonBean.getId(),
-//                            showLessonBean.getCredit(),
-//                            showLessonBean.getRoom(),
-//                            showLessonBean.getTeacher(),
-//                            days
-//                        )
-//                    )
-                })
-                val lessonTextView = lessonLinearLayout.findViewById<View>(R.id.lessonTextView) as TextView
-                lessonTextView.text = showLessonBean.getName().toString() + "\n@" + showLessonBean.getRoom()
-                lessonTextView.width = gridWidth
-                lessonTextView.height = gridHeight * (time[1].toInt() - time[0].toInt() + 1)
-                val shape = resources.getDrawable(R.drawable.grid_background) as GradientDrawable
-                shape.setColor(ColorUtils.setAlphaComponent(resources.getColor(showLessonBean.bgColor), 192))
-                lessonTextView.setBackgroundDrawable(shape)
-                val rowSpec = GridLayout.spec(time[0].toInt() - 1, time[1].toInt() - time[0].toInt() + 1)
-                val columnSpec = GridLayout.spec(3)
-                gridLayout.addView(lessonLinearLayout, GridLayout.LayoutParams(rowSpec, columnSpec))
+                drawGrid(showLessonBean, showLessonBean.getDays().getW3(), 3)
             }
             if (showLessonBean.getDays().getW4() != null && showLessonBean.getDays().getW4() != "-") {
                 Log.d(TAG, "showSyllabus: 周四" + showLessonBean.getDays().getW4())
-                val time: List<String> = showLessonBean.getDays().getW4().split("-")
-                val lessonLinearLayout = LayoutInflater.from(activity)
-                    .inflate(R.layout.lesson_grid, null, false) as MaterialRippleLayout
-                val lessonRippleLayout: MaterialRippleLayout = lessonLinearLayout.findViewById<View>(R.id.lessonInfoRipple) as MaterialRippleLayout
-                lessonRippleLayout.setOnClickListener(View.OnClickListener {
-                    val days: String = showLessonBean.getDuration().toString() + "周\n" + showLessonBean.getDays().toString()
-//                    mActivity.startActivity(
-//                        LessonInfoActivity.getIntent(
-//                            mActivity,
-//                            showLessonBean.getBgColor(),
-//                            showLessonBean.getName(),
-//                            showLessonBean.getId(),
-//                            showLessonBean.getCredit(),
-//                            showLessonBean.getRoom(),
-//                            showLessonBean.getTeacher(),
-//                            days
-//                        )
-//                    )
-                })
-                val lessonTextView = lessonLinearLayout.findViewById<View>(R.id.lessonTextView) as TextView
-                lessonTextView.text = showLessonBean.getName().toString() + "\n@" + showLessonBean.getRoom()
-                lessonTextView.width = gridWidth
-                lessonTextView.height = gridHeight * (time[1].toInt() - time[0].toInt() + 1)
-                val shape = resources.getDrawable(R.drawable.grid_background) as GradientDrawable
-                shape.setColor(
-                    ColorUtils.setAlphaComponent(
-                        resources.getColor(
-                            showLessonBean.getBgColor()
-                        ), 192
-                    )
-                )
-                lessonTextView.setBackgroundDrawable(shape)
-                val rowSpec =
-                    GridLayout.spec(time[0].toInt() - 1, time[1].toInt() - time[0].toInt() + 1)
-                val columnSpec = GridLayout.spec(4)
-                gridLayout.addView(lessonLinearLayout, GridLayout.LayoutParams(rowSpec, columnSpec))
+                drawGrid(showLessonBean, showLessonBean.getDays().getW4(), 4)
             }
             if (showLessonBean.getDays().getW5() != null && showLessonBean.getDays().getW5() != "-") {
                 Log.d(TAG, "showSyllabus: 周五" + showLessonBean.getDays().getW5())
-                val time: List<String> = showLessonBean.getDays().getW5().split("-")
-                val lessonLinearLayout = LayoutInflater.from(activity)
-                    .inflate(R.layout.lesson_grid, null, false) as MaterialRippleLayout
-                val lessonRippleLayout: MaterialRippleLayout = lessonLinearLayout.findViewById<View>(R.id.lessonInfoRipple) as MaterialRippleLayout
-                lessonRippleLayout.foregroundGravity = Gravity.CENTER
-                lessonRippleLayout.setOnClickListener(View.OnClickListener {
-                    val days: String =
-                        showLessonBean.getDuration().toString() + "周\n" + showLessonBean.getDays()
-                            .toString()
-//                    mActivity.startActivity(
-//                        LessonInfoActivity.getIntent(
-//                            mActivity,
-//                            showLessonBean.getBgColor(),
-//                            showLessonBean.getName(),
-//                            showLessonBean.getId(),
-//                            showLessonBean.getCredit(),
-//                            showLessonBean.getRoom(),
-//                            showLessonBean.getTeacher(),
-//                            days
-//                        )
-//                    )
-                })
-                val lessonTextView =
-                    lessonLinearLayout.findViewById<View>(R.id.lessonTextView) as TextView
-                lessonTextView.text = showLessonBean.getName().toString() + "\n@" + showLessonBean.getRoom()
-                lessonTextView.width = gridWidth
-                lessonTextView.height = gridHeight * (time[1].toInt() - time[0].toInt() + 1)
-                val shape = resources.getDrawable(R.drawable.grid_background) as GradientDrawable
-                shape.setColor(ColorUtils.setAlphaComponent(resources.getColor(showLessonBean.getBgColor()), 192))
-                lessonTextView.setBackgroundDrawable(shape)
-                val rowSpec = GridLayout.spec(time[0].toInt() - 1, time[1].toInt() - time[0].toInt() + 1)
-                val columnSpec = GridLayout.spec(5)
-                gridLayout.addView(lessonRippleLayout, GridLayout.LayoutParams(rowSpec, columnSpec))
+                drawGrid(showLessonBean, showLessonBean.getDays().getW5(), 5)
             }
             if (showLessonBean.getDays().getW6() != null && showLessonBean.getDays().getW6() != "-") {
                 Log.d(TAG, "showSyllabus: 周六" + showLessonBean.getDays().getW6())
+                drawGrid(showLessonBean, showLessonBean.getDays().getW6(), 6)
             }
         }
+    }
+
+    private fun drawGrid(showLessonBean: Lesson, wString: String, start: Int) {
+        val time: List<String> = wString.split("-")
+        val lessonLinearLayout = LayoutInflater.from(activity).inflate(R.layout.lesson_grid, null, false) as MaterialRippleLayout
+        val lessonRippleLayout: MaterialRippleLayout = lessonLinearLayout.findViewById(R.id.lessonInfoRipple) as MaterialRippleLayout
+        lessonRippleLayout.foregroundGravity = Gravity.CENTER
+        val lessonTextView = lessonLinearLayout.findViewById<TextView>(R.id.lessonTextView)
+        lessonTextView.text = showLessonBean.getName().toString() + "\n@" + showLessonBean.getRoom()
+        lessonTextView.width = gridWidth
+        lessonTextView.height = gridHeight * (time[1].toInt() - time[0].toInt() + 1)
+        val shape = resources.getDrawable(R.drawable.grid_background) as GradientDrawable
+        shape.setColor(ColorUtils.setAlphaComponent(resources.getColor(showLessonBean.bgColor), 192))
+        lessonTextView.setBackgroundDrawable(shape)
+        val rowSpec = GridLayout.spec(time[0].toInt() - 1, time[1].toInt() - time[0].toInt() + 1)
+        val columnSpec = GridLayout.spec(start)
+        gridLayout.addView(lessonRippleLayout, GridLayout.LayoutParams(rowSpec, columnSpec))
     }
 
     companion object {
