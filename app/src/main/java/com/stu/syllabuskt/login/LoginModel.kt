87d1fb77084b70af80cf53d58876a3c8bf9aff9d
yuan <yuansssf@gmail.com>
2020-12-16 10:47:49 +0800

    登录兜底策略

--- app/src/main/java/com/stu/syllabuskt/login/LoginModel.kt
+++ app/src/main/java/com/stu/syllabuskt/login/LoginModel.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.util.Log
 import com.stu.syllabuskt.R
 import com.stu.syllabuskt.StuContext
+import com.stu.syllabuskt.api.OfficialAccountApi
 import com.stu.syllabuskt.api.RetrofitProvider
 import com.stu.syllabuskt.api.YiBanApi
 import com.stu.syllabuskt.bean.YiBanTimeTable
@@ -29,6 +30,7 @@ class LoginModel(private val mContext: Context) {
     private val loginURL = "https://yiban.stu.edu.cn/web/Account/Login"
 
     private val yiBanApi: YiBanApi = RetrofitProvider.getYiBanRetrofit(mContext).create(YiBanApi::class.java)
+    private val officialAccountApi: OfficialAccountApi = RetrofitProvider.getOfficialWCRetrofit(mContext).create(OfficialAccountApi::class.java)
 
     fun login(account: String, password: String, loginListener: LoginListener) {
         if (!verify(account, password)) {
@@ -41,7 +43,7 @@ class LoginModel(private val mContext: Context) {
                 .enqueue(object : retrofit2.Callback<String> {
 
                     override fun onFailure(call: retrofit2.Call<String>, t: Throwable) {
-                        loginListener.onFailure("网络出错，请稍后重试")
+                        loginWithWCOfficialAccountApi(account, password, loginListener)
                         Log.e(TAG, t.message ?: "")
                     }
 
@@ -49,8 +51,6 @@ class LoginModel(private val mContext: Context) {
                         call: retrofit2.Call<String>,
                         response: retrofit2.Response<String>
                     ) {
-//                        Log.i(TAG, response.body().toString())
-                        // TODO: 2020/12/4 Android 6.0.1会crash
                         val verifyToken = Jsoup.parse(response.body().toString())
                             .getElementsByAttributeValue("name", "__RequestVerificationToken")
                             .first()
@@ -68,12 +68,6 @@ class LoginModel(private val mContext: Context) {
                                             call: Call<YiBanToken>,
                                             response: Response<YiBanToken>
                                         ) {
-                                            Log.i(TAG, "" + response.body()?.vid)
-                                            Log.i(TAG, "" + response.body()?.timestamp)
-                                            Log.i(TAG, response.body()?.app ?: "")
-                                            Log.i(TAG, response.body()?.nonce ?: "")
-                                            Log.i(TAG, response.body()?.token ?: "")
-                                            StuContext.getDBService().writeBaseUserInfo(mContext, account, password)
                                             var token = ""
                                             try {
                                                 token = URLEncoder.encode(response.body()?.token ?: "", "UTF-8")
@@ -85,6 +79,7 @@ class LoginModel(private val mContext: Context) {
                                                     call: Call<YiBanTimeTable>,
                                                     response: Response<YiBanTimeTable>
                                                 ) {
+                                                    StuContext.getDBService().writeBaseUserInfo(mContext, account, password)
                                                     response.body()?.table?.forEach { it ->
                                                         StuContext.getDBService().writeSyllabus(mContext, account, it, SyllabusSourceType.Official)
                                                     }
@@ -96,7 +91,7 @@ class LoginModel(private val mContext: Context) {
                                                     call: Call<YiBanTimeTable>,
                                                     t: Throwable
                                                 ) {
-
+                                                    loginWithWCOfficialAccountApi(account, password, loginListener)
                                                 }
                                             })
                                         }
@@ -106,20 +101,38 @@ class LoginModel(private val mContext: Context) {
                                             t: Throwable
                                         ) {
                                             Log.e(TAG, t.message ?: "")
-                                            loginListener.onFailure("网络出错，请稍后重试")
+                                            loginWithWCOfficialAccountApi(account, password, loginListener)
                                         }
                                     })
                                 }
 
                                 override fun onFailure(call: Call<String>, t: Throwable) {
                                     Log.e(TAG, t.message ?: "")
-                                    loginListener.onFailure("网络出错，请稍后重试")
+                                    loginWithWCOfficialAccountApi(account, password, loginListener)
                                 }
                             })
                     }
 
                 })
         }
+    }
+
+    /**
+     * 兜底策略：使用另一数据接口实现登录
+     */
+    private fun loginWithWCOfficialAccountApi(account: String, password: String, loginListener: LoginListener) {
+        officialAccountApi.login(account, password, "登录", "", "")
+            .enqueue(object : retrofit2.Callback<String> {
+                override fun onResponse(call: Call<String>, response: Response<String>) {
+                    StuContext.getDBService().writeBaseUserInfo(mContext, account, password)
+                    StuContext.getSharePreferences(mContext).edit().putString(SyllabusContainerFragment.CurrentSemesterKey, "Non-existent").apply()
+                    loginListener.onSuccess()
+                }
+
+                override fun onFailure(call: Call<String>, t: Throwable) {
+                    loginListener.onFailure("账号密码错误或网络状态出错")
+                }
+            })
     }
 
     /**
