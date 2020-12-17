dd8e7e9ccf2328a9c070f39cdf14f0864896dedd
yuan <yuansssf@gmail.com>
2020-12-17 21:09:09 +0800

    set&get version info

--- app/src/main/java/com/stu/syllabuskt/App.java
+++ app/src/main/java/com/stu/syllabuskt/App.java
@@ -3,11 +3,16 @@ package com.stu.syllabuskt;
 import android.annotation.SuppressLint;
 import android.app.Application;
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 
 /**
  * Create by yuan on 2020/12/13
  */
 public class App extends Application {
+
+    public static int versionCode = Integer.MAX_VALUE;
+    public static String versionName = "";
 
     @SuppressLint("StaticFieldLeak")
     private static Context context;
@@ -16,9 +21,20 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         context = getApplicationContext();
+        initVersionInfo();
     }
 
     public static Context getContext() {
         return context;
+    }
+
+    private void initVersionInfo() {
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            versionCode = packageInfo.versionCode;
+            versionName= packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
