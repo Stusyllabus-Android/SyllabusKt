962e721921d7f62787df6faca55762f92c578b99
yuan <yuansssf@gmail.com>
2020-12-17 20:52:10 +0800

    给当前周text设置背景

--- app/src/main/java/com/stu/syllabuskt/syllabus/ext/SetWeekAdapter.kt
+++ app/src/main/java/com/stu/syllabuskt/syllabus/ext/SetWeekAdapter.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.stu.syllabuskt.R
+import com.stu.syllabuskt.StuContext
+import com.stu.syllabuskt.syllabus.SyllabusContainerFragment
 
 /**
  * Create by yuan on 2020/12/14
@@ -22,6 +24,8 @@ class SetWeekAdapter(val context: Context) : RecyclerView.Adapter<SetWeekAdapter
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.textView.apply {
             text = "${position + 1}"
+            val setWeek = StuContext.getSharePreferences(context).getInt(SyllabusContainerFragment.SetWeekKey, -1)
+            if (setWeek == position || (setWeek == -1 && position == 0)) background = context.resources.getDrawable(R.color.material_grey_200)
             setOnClickListener { onItemClickListener?.onItemClickListener(position) }
         }
     }
