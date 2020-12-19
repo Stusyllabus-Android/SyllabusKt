cf42a093466a30491dba7f15aea9d0dd87aa0853
yuan <yuansssf@gmail.com>
2020-12-19 10:59:46 +0800

    接入umeng进行数据统计

--- app/src/main/java/com/stu/syllabuskt/App.java
+++ app/src/main/java/com/stu/syllabuskt/App.java
@@ -5,11 +5,17 @@ import android.app.Application;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.util.Log;
+
+import com.umeng.analytics.MobclickAgent;
+import com.umeng.commonsdk.UMConfigure;
 
 /**
  * Create by yuan on 2020/12/13
  */
 public class App extends Application {
+
+    private String TAG = "SyllabusKt";
 
     public static int versionCode = Integer.MAX_VALUE;
     public static String versionName = "";
@@ -20,8 +26,13 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.i(TAG, "Application onCreate");
         context = getApplicationContext();
         initVersionInfo();
+        UMConfigure.preInit(this, "5fda1746dd2891533921a2f2", "Umeng");
+        UMConfigure.init(this, "5fda1746dd2891533921a2f2", "Umeng", UMConfigure.DEVICE_TYPE_PHONE, "");
+        MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.AUTO);
+        UMConfigure.setLogEnabled(true);
     }
 
     public static Context getContext() {
